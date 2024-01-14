1c0e733f5379c98c035cee973b8ef0ef7d38efa1
harutyunyanart <artur.harutyunyan908@gmail.com>
2024-01-14 12:48:35 +0400

    added sity,number,state,mobile Number locators handling

--- src/test/java/test/RegistrationTest.java
+++ src/test/java/test/RegistrationTest.java
@@ -16,28 +16,28 @@ public class RegistrationTest extends BaseTest {
         SignUpLoginPage signUpLoginPage = new SignUpLoginPage(driver);
         signUpLoginPage.clickSignUpButton();
         signUpLoginPage.sendSignUpName("Artur");
-        signUpLoginPage.sendSignUpEmailAddress("ArturH@mail.ru");
+        signUpLoginPage.sendSignUpEmailAddress("ArturHAA@mail.ru");
         SignUpPage signUpPage = signUpLoginPage.clickSignUpButton();
         signUpPage.sendLoginPassword("password");
         signUpPage.clickOnDayOfBirth();
         signUpPage.selectDayOfBirth("12");
-
         signUpPage.clickOnDayOfMonth();
         signUpPage.selectMonthOfBirth("April");
-
         signUpPage.clickOnYearOfDay();
         signUpPage.selectYearOfDay("1999");
-
-
-
-        signUpPage.setFirstName("FirstNume");
-        signUpPage.setLastName("LastNume");
-
+        signUpPage.setOffers(true);
+        signUpPage.setFirstName("Art");
+        signUpPage.setLastName("Har");
         signUpPage.setAddress("AddressStreet2");
         signUpPage.setAddress2("Street3");
         signUpPage.setState("Oklahoma");
+        signUpPage.setCity("Glendale");
+        signUpPage.setZipCode("91020");
+        signUpPage.setMobileNumber("043322133");
+        signUpPage.clickCreateAccount();
+        signUpPage.clickContinueButton();
+        signUpPage.clickDeleteAccountButton();
 
-//        signUpPage.setCity("washington");
 
 
 
